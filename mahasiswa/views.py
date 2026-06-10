90ed6fafbdfdb77bdcc3ab7cfd709cbc4a3e195a
Zacky7518 <sambora7518@gmail.com>
2026-06-10 13:26:26 +0700

    tambah fitur CRUD

--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Mahasiswa
 from django.contrib.auth.decorators import login_required
 
@@ -8,10 +8,37 @@ def index(request):
         'deskripsi': 'Contoh halaman index menggunakan Django templates dan static files.'
     }
     return render(request, 'mahasiswa/index.html', context)
-# Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def daftar_mahasiswa(request):
-    # Mengambil semua data mahasiswa dari database untuk dikirim ke template
     mahasiswas = Mahasiswa.objects.all()
     return render(request, 'mahasiswa/daftar.html', {'mahasiswas': mahasiswas})
+
+@login_required(login_url='/accounts/login/')
+def tambah_mahasiswa(request):
+    if request.method == 'POST':
+        nim = request.POST.get('nim')
+        nama = request.POST.get('nama')
+        programstudi = request.POST.get('programstudi')
+        angkatan = request.POST.get('angkatan')
+        Mahasiswa.objects.create(nim=nim, nama=nama, programstudi=programstudi, angkatan=angkatan)
+        return redirect('daftar_mahasiswa')
+    return render(request, 'mahasiswa/tambah.html')
+
+@login_required(login_url='/accounts/login/')
+def edit_mahasiswa(request, id):
+    mhs = get_object_or_404(Mahasiswa, id=id)
+    if request.method == 'POST':
+        mhs.nim = request.POST.get('nim')
+        mhs.nama = request.POST.get('nama')
+        mhs.programstudi = request.POST.get('programstudi')
+        mhs.angkatan = request.POST.get('angkatan')
+        mhs.save()
+        return redirect('daftar_mahasiswa')
+    return render(request, 'mahasiswa/edit.html', {'mhs': mhs})
+
+@login_required(login_url='/accounts/login/')
+def hapus_mahasiswa(request, id):
+    mhs = get_object_or_404(Mahasiswa, id=id)
+    mhs.delete()
+    return redirect('daftar_mahasiswa')
