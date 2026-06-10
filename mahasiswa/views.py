577ffbb8d5652d816c0b36090fb4584432f86fcc
Zacky7518 <sambora7518@gmail.com>
2026-06-10 12:46:43 +0700

    fix urls redirect

--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from .models import Mahasiswa
 from django.contrib.auth.decorators import login_required
 
+def index(request):
+    context = {
+        'judul': 'Halo Mahasiswa',
+        'deskripsi': 'Contoh halaman index menggunakan Django templates dan static files.'
+    }
+    return render(request, 'mahasiswa/index.html', context)
+# Create your views here.
+
 @login_required(login_url='/accounts/login/')
 def daftar_mahasiswa(request):
     # Mengambil semua data mahasiswa dari database untuk dikirim ke template
